83b62e70e94f3a575ff8d3077fc7c7a4e5989fff
Marek Labos <keraml@gmail.com>
2012-08-29 15:08:01 +0200

    monkey patch weird bug in admin

--- app/controllers/refinery/calendar/admin/calendar_controller.rb
+++ app/controllers/refinery/calendar/admin/calendar_controller.rb
@@ -2,6 +2,9 @@ module Refinery
   module Calendar
     module Admin
       class CalendarController < ::Refinery::AdminController
+        # reinclude because weird bug when searching? method is sometimes not present ;(
+        include Refinery::Admin::BaseController
+
         include CalendarHelper
       end
     end
