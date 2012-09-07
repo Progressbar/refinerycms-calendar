dc1aeb09e97b83a8370031e7699c6298ebf3cae4
Marek Labos <keraml@gmail.com>
2012-09-08 01:11:04 +0200

    refactoring helpers

--- app/helpers/refinery/calendar/admin/calendar_helper.rb
+++ app/helpers/refinery/calendar/admin/calendar_helper.rb
@@ -3,22 +3,22 @@ module Refinery
     module Admin
       module CalendarHelper
 
-        def user_can_modify_events
+        def user_can_modify_events?
           (current_refinery_user.has_role?(:superuser) || current_refinery_user.authorized_plugins.include?("refinerycms_calendar"))
         end
 
-        def user_can_modify_event (event)
+        def user_can_modify_event? (event)
           (current_refinery_user.has_role?(:superuser) || 
             (current_refinery_user.authorized_plugins.include?("refinerycms_calendar") && (event.user_id == current_refinery_user.id || ::Refinery::Calendar.refinery_user_can_manage_events)))
         end
 
-      	def user_can_modify_categories
+      	def user_can_modify_categories?
           (current_refinery_user.has_role?(:superuser) || 
             (current_refinery_user.authorized_plugins.include?("refinerycms_calendar") && 
              ::Refinery::Calendar.refinery_user_can_manage_categories))
       	end
 
-      	def user_can_modify_places
+      	def user_can_modify_places?
           (current_refinery_user.has_role?(:superuser) || 
             (current_refinery_user.authorized_plugins.include?("refinerycms_calendar") && 
               ::Refinery::Calendar.refinery_user_can_manage_places))
