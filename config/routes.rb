c11f1c3bd344b722c2a57b3551750120ee8e1c5e
Marek Labos <keraml@gmail.com>
2012-09-08 19:33:08 +0200

    cleaning code

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Refinery::Core::Engine.routes.append do
   namespace :calendar do
-    root :to => "events#index"
+    root :to => 'events#index'
     
     get 'archive/:year(/:month)' => 'events#archive', :as => 'archive'
     get 'category/:id' => 'categories#show', :as => 'category'
@@ -12,7 +12,7 @@ Refinery::Core::Engine.routes.append do
 
   # Admin routes
   
-  namespace :calendar, :path => '' do    
+  namespace :calendar, :path => '' do
     namespace :admin, :path => 'refinery/calendar' do
       resources :events, :except => :show
       resources :places, :except => :show
