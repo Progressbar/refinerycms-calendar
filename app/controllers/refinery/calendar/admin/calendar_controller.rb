b86bd73b98bf40641a248bdc86c10fed10882a65
Marek Labos <keraml@gmail.com>
2012-09-14 17:14:18 +0200

    group events by start_date in admin

--- app/controllers/refinery/calendar/admin/calendar_controller.rb
+++ app/controllers/refinery/calendar/admin/calendar_controller.rb
@@ -2,7 +2,21 @@ module Refinery
   module Calendar
     module Admin
       class CalendarController < ::Refinery::AdminController
-      	include CalendarHelper
+		include CalendarHelper
+
+		protected
+
+		def group_by_date(records, date=:created_at)
+	        new_records = []
+
+	        records.each do |record|
+	          key = record[date].strftime("%Y-%m-%d")
+	          record_group = new_records.collect{|records| records.last if records.first == key }.flatten.compact << record
+	          (new_records.delete_if {|i| i.first == key}) << [key, record_group]
+	        end
+
+	        new_records
+	    end
       end
     end
   end
