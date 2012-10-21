e937a3ee1767694a2bc65f76eac9299dfb9cc973
Marek Labos <keraml@gmail.com>
2012-10-21 04:45:14 +0200

    fix route to refinery/calendar

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,21 @@
 Refinery::Core::Engine.routes.append do
   namespace :calendar do
     root :to => 'events#index'
-    
+
     get 'archive/:year(/:month)' => 'events#archive', :as => 'archive'
     get 'category/:id' => 'categories#show', :as => 'category'
     get 'place/:id' => 'places#show', :as => 'place'
 
     resources :events, :path => '', :only => [:show]
-    
+
   end
 
   # Admin routes
-  
+
   namespace :calendar, :path => '' do
     namespace :admin, :path => 'refinery/calendar' do
+      root :to => "events#index"
+
       resources :events, :except => :show
       resources :places, :except => :show
       resources :categories, :except => :show
